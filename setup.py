196a8778109a6bb808057caafd54c52b0827e182
advil <rawlins@gmail.com>
2021-02-24 09:12:32 -0500

    Change version style to use semver, mark as 1.0.0
    Just call the version as of 2019 1.0.0, rather than worry about any
    post-hoc versioning for previous updates. (There have been roughly two
    releases, one in 2016 for the original paper, and then one in 2017 for
    the IWCS paper; with a few followup bugfixes later.)

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='predpatt',
-      version='1.0',
+      version='1.0.0',
       description='Multilingual predicate-argument extraction from universal dependency syntax.',
       packages=['predpatt', 'predpatt.util'],
       install_requires=['concrete',
